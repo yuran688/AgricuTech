d3ee2d717ca7d600b2af5e9c5ff39b5a6c1eb748
yuran688 <62353952+yuran688@users.noreply.github.com>
2025-08-13 21:07:30 +0200

    Add files via upload

--- campanhas.js
+++ campanhas.js
@@ -2,6 +2,6 @@ import express from 'express';
 import { criarCampanha } from '../controllers/campanhaController.js';
 const router = express.Router();
 
-router.post('/', criarcamanhas);
+router.post('/', criarCampanha);
 
 export default router;
